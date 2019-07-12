2bc868d8d722cba32ea67053a47ee23895da2a87
bee1don <33287160+bee1don@users.noreply.github.com>
2019-07-12 10:40:08 +0100

    Update QnABot.cs

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -48,7 +48,7 @@ namespace Microsoft.BotBuilderSamples
             }
             else
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found sorry."), cancellationToken);
             }
         }
 
